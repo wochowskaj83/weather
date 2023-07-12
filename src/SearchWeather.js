573f6f9be3676f36dce0c3c445432a9c6a19849e
wochowskaj83 <wochowskaj@gmail.com>
2023-07-12 13:48:49 +0200

    rozwiazanie zadania

---
+++ src/SearchWeather.js
@@ -0,0 +1,15 @@
+const SearchWeather = (props) => {
+    return (
+        <div className="searchWeather">  
+            <form>
+                <label htmlFor="searchWeather">Szukaj:</label>
+                <input 
+                type="text" 
+                id="searchWeather" 
+                onChange={(e)=>props.filterWeather(e.target.value)}/>
+            </form>
+        </div>
+    )
+}
+
+export default SearchWeather
